5c6217e05aa1ed43dcd8b633834ec2551cf417e9
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-04-14 08:42:00 -0400

    update

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 
 import requests
+from datetime import datetime
 
 USERNAME = "jaydunb12"
 TOKEN = "sijnbadjfnbkljadnfbjdnb"
+GRAPH_ID = "graph1"
 
 
 pixela_api = "https://pixe.la/v1/users"
@@ -24,12 +26,28 @@ headers = {
 graph_api = f"{pixela_api}/{USERNAME}/graphs"
 
 graph_config = {
-    "id": "graph1",
+    "id": GRAPH_ID,
     "name": "Coding Tracker",
     "unit": "minute",
     "type": "int",
     "color": "sora"
 }
 
+# graph_update = f"{graph_api}/graph1"
+
+pix_creation = f"{pixela_api}/{USERNAME}/graphs/{GRAPH_ID}"
+
+today = datetime.now()
+
+graph_body_update = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": "35"
+}
+
+
 response = requests.post(url=graph_api, json=graph_config, headers=headers)
+update = requests.post(url=pix_creation, json=graph_body_update, headers=headers)
+
 print(response.text)
+print(update.text)
+print(today)
